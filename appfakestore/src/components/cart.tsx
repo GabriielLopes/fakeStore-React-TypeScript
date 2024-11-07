b2dfa6742247d8c6cda34ee5e8704c27a99c3e54
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-11-07 13:06:42 -0300

    Add função de remover itens do carrinho.

--- appfakestore/src/components/cart.tsx
+++ appfakestore/src/components/cart.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
+import { useDispatch } from 'react-redux';
 
 import '../styles/cart.css';
 import { State } from '../interfaces/state';
 import axios from '../services/axios';
 import { Product } from '../interfaces/product';
+import { removeProduct } from '../store/modules/carrinho/actionCreators';
 
 export function Cart() {
+  const dispatch: Dispatch<any> = useDispatch();
+
   const cart = useSelector<State>((state) => state.carrinho) as Carrinho;
   const idsProductsInCart = cart.products?.map((productsInCart) => productsInCart.productId);
 
@@ -22,8 +27,16 @@ export function Cart() {
     getData();
   }, [idsProductsInCart]);
 
+  const handleDelete = (product: ProductCart): void => {
+    if (!product) return;
+    dispatch(removeProduct(product));
+  };
+
   return (
-    <div className="container-cart visible">
+    <div className="container-cart">
+      {productsInCart.length <= 0 ? (
+        <h3>Seu carrinho está vazio!</h3>
+      ): ''}
       {productsInCart.map((produto) => (
         <>
           <div className="cart-content">
@@ -46,6 +59,16 @@ export function Cart() {
                 </button>
               </div>
             </small>
+            <button
+              onClick={() =>
+                handleDelete({
+                  productId: produto.id as number,
+                  quantity: 1,
+                })
+              }
+            >
+              Deletar
+            </button>
           </div>
           <hr />
         </>
--- appfakestore/src/store/modules/carrinho/reducer.ts
+++ appfakestore/src/store/modules/carrinho/reducer.ts
@@ -12,33 +12,37 @@ const initialState: Carrinho = {
   ],
 };
 
-
 const reducer = (state: Carrinho = initialState, action: CarrinhoAction): Carrinho => {
   switch (action.type) {
-    case actionsTypes.ADD_CARRINHO:
+    case actionsTypes.ADD_CARRINHO: {
       const newCarrinho = state;
       if (newCarrinho) {
         newCarrinho.products?.push({
           productId: action.product.productId,
-          quantity: 1
+          quantity: 1,
         });
       }
       const products = newCarrinho.products;
-      console.log(action)
+      console.log(action);
       return {
         ...state,
         products,
       };
+    }
 
-    /*
-    case actionsTypes.REMOVE_CARRINHO:
-      const carrinhoAtualizado: Carrinho[] = state.carrinhos.filter(
-        (carrinho) => carrinho.id !== action.carrinho.id,
+    case actionsTypes.REMOVE_CARRINHO: {
+      const newCarrinho = state;
+      const productIndex = newCarrinho.products?.findIndex(
+        (product) => product.productId === action.product.productId,
       );
+      if (typeof productIndex === 'number') {
+        newCarrinho.products?.splice(productIndex, 1);
+      }
       return {
         ...state,
-        carrinhos: carrinhoAtualizado,
-      };*/
+        products: newCarrinho.products,
+      };
+    }
   }
   return state;
 };
