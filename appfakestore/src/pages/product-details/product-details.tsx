948e2d8137fa744601ffa94619fdc163fed9ca59
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-11-08 16:55:14 -0300

    Add função add item carrinho na page details

--- appfakestore/src/pages/product-details/product-details.tsx
+++ appfakestore/src/pages/product-details/product-details.tsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Product } from '../../interfaces/product';
 import axios from '../../services/axios';
 import { useParams } from 'react-router-dom';
+import { Dispatch } from 'redux';
 
 import '../../styles/product-details.css';
+import { formatarValor } from '../../utils/formatarValor';
+import produtoExisteNoCarrinho from '../../utils/produtoExisteNoCarrinho';
+import { State } from '../../interfaces/state';
+import { Carrinho } from '../../store/modules/type';
+import aumentarQtdeItemCarrinho from '../../utils/aumentarQtdeItemCarrinho';
+import Swal from 'sweetalert2';
+import { addProduct } from '../../store/modules/carrinho/actionCreators';
 
 export default function ProductDetails() {
+  const dispatch: Dispatch<any> = useDispatch();
+
+  const cart = useSelector<State>((state) => state.carrinho) as Carrinho;
   const [produto, setProduto] = useState<Product>({});
   const [produtos, setProdutos] = useState<Product[]>([]);
   const { id } = useParams();
@@ -26,12 +38,42 @@ export default function ProductDetails() {
     getData();
   }, []);
 
+  const addItemCarrinho = (product: Product): void => {
+    if (!product) return;
+    if (produtoExisteNoCarrinho(cart, product.id as number)) {
+      if (typeof product.id !== 'number') return;
+      aumentarQtdeItemCarrinho({ productId: product.id, quantity: 1 }, dispatch);
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        heightAuto: true,
+        title: 'Added another quantity to the cart.',
+        showConfirmButton: false,
+        timer: 800,
+      });
+      return;
+    }
+    dispatch(
+      addProduct({
+        productId: product.id ?? 0,
+        quantity: 1,
+      }),
+    );
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Product added to cart',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
   return (
     <div>
       <section className="section section-white">
         <br />
         <div className="text-content product-detail">
-            <h4>{produto.title}</h4>
+          <h4>{produto.title}</h4>
           <div className="grid-img">
             <img src={produto.image} alt={produto.description} className="img-destaque2" />
             <p>
@@ -49,7 +91,7 @@ export default function ProductDetails() {
               <div className="controls">
                 <button className="btn comprar-agora">BUY NOW!</button>
 
-                <button className="btn add-carrinho">
+                <button className="btn add-carrinho" onClick={() => addItemCarrinho(produto)}>
                   ADD CART
                   <i className="bx bxs-cart-add" />
                 </button>
@@ -61,7 +103,7 @@ export default function ProductDetails() {
       </section>
 
       <h2>See other products</h2>
-      <section className="section section-all-products products-details">
+      <section className="section home section-all-products">
         <div className="grid-produtos">
           {produtos.map((produto) => (
             <div className="grid-produtos-content">
@@ -81,10 +123,10 @@ export default function ProductDetails() {
               </p>
               <br />
 
-              <strong>Price: {produto.price}</strong>
+              <strong>Price: {formatarValor.format(produto.price as number)}</strong>
               <div className="controls">
-                <button className="btn add-carrinho-small">
-                  <i className="bx bxs-cart-add" />
+                <button className="btn add-carrinho-small" onClick={() => addItemCarrinho(produto)}>
+                  Add <i className="bx bxs-cart-add" />
                 </button>
               </div>
             </div>
